2c20624e0e45d143af91fe5d8fc3ff21e20b3048
Kingfree <anytjf@gmail.com>
2016-11-22 20:45:06 +0800

    hi@kingfree.moe

--- assets/js/tableofcontents.js
+++ assets/js/tableofcontents.js
@@ -1,21 +1,22 @@
 $(document).ready(function () {
-  var category = $("#category");
+
+  var category = $('#category');
 
   $("h2, h3").each(function (i, item) {
-    if ($(item).attr("id") == undefined) {
+    if ($(item).attr('id') == undefined) {
       $(item).attr("id", "section" + i);
     }
     var itemId = $(item).attr("id");
     var className = $(item).get(0).localName;
 
     if (className == 'h2') {
-      category = $("#category");
-      category.append('<li>' + '<a href="#' + itemId + '">' + $(this).text() + '</a> <ul id="ulsection' + i + '" class="nav"></ul></li>');
-      category = $("#ulsection" + i);
+      category = $('#category');
+      category.append('<li><a href="#' + itemId + '">' + $(this).text() + '</a><ul id="ulSection' + i + '" class="nav"></ul></li>');
+      category = $('#ulSection' + i);
     } else if (className == 'h3') {
-      category.append('<li>' + '<a href="#' + itemId + '">' + $(this).text() + '</a>' + '</li>');
+      category.append('<li><a href="#' + itemId + '">' + $(this).text() + '</a></li>');
     }
   });
 
-  $('body').scrollspy({target: ".bs-docs-sidebar"});
+  $('body').scrollspy({target: '.bs-docs-sidebar'});
 });
